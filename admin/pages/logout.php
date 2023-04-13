8f128761706d24f89cead3053141ba9aae2a5ec1
M Furqan Afzal <muhammadfurqanafzalcr@gmail.com>
2023-04-13 18:16:33 +0500

    Update logout.php

--- admin/pages/logout.php
+++ admin/pages/logout.php
@@ -4,8 +4,4 @@ if (isset($_COOKIE["admin_id"]) && !empty($_COOKIE["admin_id"]) ) {
     setcookie("admin_id", '', time() +  1, '/');
 }
 unset($_SESSION["admin_id"]);
-// echo "<pre>";
-// print_r($_SESSION);
-// print_r($_COOKIE);
-// die;
 echo "<script>window.location = '../';</script>";
