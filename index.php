a97ff0cce10e67c802d7ef920e798d853a33ba86
MUHAMMAD HAMZA <hamzafaryadbwp@gmail.com>
2023-05-05 21:47:31 +0500

    brands

--- index.php
+++ index.php
@@ -6,75 +6,28 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="assets/css/styles.css">
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css'>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
-    <title><?=$siteTitle?></title>
+    
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title><?= $siteTitle ?></title>
 </head>
 
 <body id="page-top">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 
     <?php include "components/header.php"  ?>
     <?php include "pages/$page.php"  ?>
     <?php include "components/footer.php" ?>
 
-
-    <!-- Optional JavaScript -->
-    <script>
-        var video = document.getElementById("myVideo");
-        var btn = document.getElementById("myBtn");
-
-        function myFunction() {
-            if (video.paused) {
-                video.play();
-                btn.innerHTML = "Pause";
-            } else {
-                video.pause();
-                btn.innerHTML = "Play";
-            }
-        }
-    </script>
-    <script>
-        //Get the button
-        var mybutton = document.getElementById("mybtn");
-
-        // When the user scrolls down 20px from the top of the document, show the button
-        window.onscroll = function() {
-            scrollFunction()
-        };
-
-        function scrollFunction() {
-            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                mybutton.style.display = "block";
-            } else {
-                mybutton.style.display = "none";
-            }
-        }
-
-        // When the user clicks on the button, scroll to the top of the document
-        function topFunction() {
-            document.body.scrollTop = 0;
-            document.documentElement.scrollTop = 0;
-        }
-    </script>
-    <script>
-        // Get the modal
-        var modal = document.getElementById('id01');
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
-    </script>
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="./assets/js/styles.js"></script>
+
 </body>
 
 </html>
---
+++ pages/cart.php
@@ -0,0 +1,187 @@
+<link rel="stylesheet" href="./assets/css/cart.css">
+<div id="banner">
+    <div class="px-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div class="flex flex-col items-center justify-between w-full mb-10 lg:flex-row">
+            <div class="mb-16 lg:mb-0 lg:max-w-lg lg:pr-5">
+                <div class="max-w-xl mb-6">
+                    <div>
+                    </div>
+                    <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+                        YOUR CART<br class="hidden md:block" />
+                        <span class="inline-block text-deep-purple-accent-400"></span>
+                    </h2>
+                    <p class="text-base text-gray-700 md:text-lg">
+                        This page is specially design for our product details. We care about our clients. All detail about our shop is here. you can also order from home.
+                    </p>
+                </div>
+                <!-- <div class="flex items-center space-x-3">
+                    <a href="/" class="w-32 transition duration-300 hover:shadow-lg">
+                        <img src="https://kitwind.io/assets/kometa/app-store.png" class="object-cover object-top w-full h-auto mx-auto" alt="" />
+                    </a>
+                    <a href="/" class="w-32 transition duration-300 hover:shadow-lg">
+                        <img src="https://kitwind.io/assets/kometa/google-play.png" class="object-cover object-top w-full h-auto mx-auto" alt="" />
+                    </a>
+                </div> -->
+            </div>
+            <div class="flex items-center justify-center lg:w-1/2" style="margin-top: 50px;">
+                <div class="w-2/5">
+                    <img class="object-cover" src="https://kitwind.io/assets/kometa/one-girl-phone.png" alt="" />
+                </div>
+                <div class="w-5/12 -ml-16 lg:-ml-32">
+                    <img class="object-cover" src="https://kitwind.io/assets/kometa/two-girls-phone.png" alt="" />
+                </div>
+            </div>
+        </div>
+        <a href="/" aria-label="Scroll down" class="flex items-center justify-center w-10 h-10 mx-auto text-gray-600 duration-300 transform border border-gray-400 rounded-full hover:text-deep-purple-accent-400 hover:border-deep-purple-accent-400 hover:shadow hover:scale-110">
+            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="currentColor">
+                <path d="M10.293,3.293,6,7.586,1.707,3.293A1,1,0,0,0,.293,4.707l5,5a1,1,0,0,0,1.414,0l5-5a1,1,0,1,0-1.414-1.414Z"></path>
+            </svg>
+        </a>
+    </div>
+</div>
+<div class="cart">
+    <div class="container">
+        <div class="grid_12">
+        </div>
+        <ul class="items">
+            <li class="grid_4 item">
+                <a href="#" class="btn-remove">
+                    <i class="far fa-trash-alt"></i>
+                </a>
+                <div class="preview">
+                    <img src="./assets/img/slider-7.webp" />
+                </div>
+                <div class="details" data-price="15.50">
+                    <h3>
+                        Samsung
+                    </h3>
+                    <p>Galaxy Prime</p>
+
+                </div>
+                <div class="inner_container">
+
+                    <div class="col_1of2 align-center picker">
+                        <p>
+                            <a href="#" class="btn-quantity plus">
+                                <i class="fas fa-plus"></i>
+                            </a>
+                        <div class="col_1of2 quantity-text">
+                            <p><span class="current_quantity">1</span> @ £50.50</p>
+                        </div>
+                        <a href="#" class="btn-quantity minus">
+                            <i class="fas fa-minus"></i>
+                        </a>
+                        </p>
+                        <input type="hidden" class="quantity_field" name="quantity" data-price="15.50" value="1" />
+                    </div>
+                </div>
+
+            </li>
+            <li class="grid_4 item">
+                <a href="#" class="btn-remove">
+                    <i class="far fa-trash-alt"></i>
+                </a>
+                <div class="preview">
+                    <img src="./assets/img/slider-8.webp" />
+                </div>
+                <div class="details" data-price="2.49">
+                    <h3>
+                        OPPO
+                    </h3>
+                    <p>A31 Pro </p>
+
+                </div>
+                <div class="inner_container">
+
+                    <div class="col_1of2 align-center picker">
+                        <p>
+                            <a href="#" class="btn-quantity plus">
+                                <i class="fas fa-plus"></i>
+                            </a>
+                        <div class="col_1of2 quantity-text">
+                            <p><span class="current_quantity">1</span> @ £22.49</p>
+                        </div>
+                        <a href="#" class="btn-quantity minus">
+                            <i class="fas fa-minus"></i>
+                        </a>
+                        </p>
+                        <input type="hidden" class="quantity_field" name="quantity" data-price="2.49" value="1" />
+                    </div>
+                </div>
+
+            </li>
+            <li class="grid_4 item">
+                <a href="#" class="btn-remove">
+                    <i class="far fa-trash-alt"></i>
+                </a>
+                <div class="preview">
+                    <img src="./assets/img/slider-9.webp" />
+                </div>
+                <div class="details" data-price="8.50">
+                    <h3>
+                        Vivo
+                    </h3>
+                    <p>Y 17 Golden</p>
+
+                </div>
+                <div class="inner_container">
+
+                    <div class="col_1of2 align-center picker">
+                        <p>
+                            <a href="#" class="btn-quantity plus">
+                                <i class="fas fa-plus"></i>
+                            </a>
+                        <div class="col_1of2 quantity-text">
+                            <p><span class="current_quantity">1</span> @ £8.50</p>
+                        </div>
+                        <a href="#" class="btn-quantity minus">
+                            <i class="fas fa-minus"></i>
+                        </a>
+                        </p>
+                        <input type="hidden" class="quantity_field" name="quantity" data-price="8.50" value="1" />
+                    </div>
+                </div>
+
+            </li>
+        </ul>
+        <div class="grid_12 delivery-payment">
+            <div class="grid_6 delivery-address">
+                <h3>Delivery Address</h3>
+                <p>46 Vale Road</br>
+                    Ramsgate</p>
+            </div>
+            <div class="grid_6 payment-details">
+                <h3>Payment Card</h3>
+                <p> **** **** **** 8678</p>
+            </div>
+        </div>
+        <div class="grid_12 summary">
+            <div class="inner_container">
+                <div class="summary-content">
+                    <div class="col_1of2 meta-data">
+                        <div class="sub-total">
+                            <em>Sub Total: </em><span class="amount"></span>
+                        </div>
+                        <div class="taxes">
+                            <em>Plus VAT: </em><span class="amount">@ 20%</span>
+                        </div>
+
+                    </div>
+                    <div class="col_1of2">
+                        <div class="total">
+                            <span class="amount"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="btn-summary">
+
+                    <a href="#" class="btn-checkout btn-reverse">Continue Shopping</a>
+
+                    <a href="#" class="btn-checkout">Checkout</a>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="./assets/js/cart.js"></script>
